4d5c8e7f03d326c6aa0c2ee55e71f8df49d3874a
Vishwassolanki <55510074+Vishwassolanki@users.noreply.github.com>
2023-12-01 23:44:37 +0530

    Adding required comments

--- problem_food_truck/data/views.py
+++ problem_food_truck/data/views.py
@@ -9,7 +9,13 @@ from rest_framework import status
 
 @api_view(['GET'])
 def food_trucks_near_me(request):
+    """
+    Function for retrieving nearby food trucks by utilizing latitude and longitude from an HTTP GET request.
+    param request: HTTP GET request containing latitude and longitude parameters.
+    return: JSON response containing nearby food trucks or error message.
+    """
     try:
+        #Retrieving latitude and longitude parameters from the request
         latitude = float(request.query_params.get('latitude'))
         longitude = float(request.query_params.get('longitude'))
         # find food trucks near location based on coordinates
@@ -17,13 +23,19 @@ def food_trucks_near_me(request):
             latitude__range=(latitude - 0.01, latitude + 0.01),
             longitude__range=(longitude - 0.01, longitude + 0.01)
         )
-
+        # Serializing the retrieved food trucks data
         serializer = FoodTruckDataSerializer(trucks_near_me, many=True)
+
+        # Returning the serialized data as a JSON response
         return Response(serializer.data)
     except ValueError as e:
+        # Handling the case when non-numeric inputs are provided for latitude or longitude
         error_message = "Invalid coordinates. Latitude and longitude must be numeric."
         return Response({"error": error_message}, status=status.HTTP_400_BAD_REQUEST)
     
     except Exception as e:
+        # Handling unexpected errors and providing a generic error message
+        # You may want to log the exception for further investigation
+        # logger.error(f"An error occurred: {e}")
         error_message = "An error occurred while processing your request."
         return Response({"error": error_message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
